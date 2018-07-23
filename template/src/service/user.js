c4678c5067e0ed8b6a9b2a5f52dff8c2e630d373
yuanxuegui <yuanxuegui@163.com>
2018-07-23 14:52:53 +0800

    删除重复api引入

--- template/src/service/user.js
+++ template/src/service/user.js
@@ -1,9 +1,7 @@
-import api from '../config/api'
-
 import request from '../util/request'
 import api from '../config/api'
 
-export function login(data) {
+export function login (data) {
   return request({
     url: api.loginUrl,
     method: 'post',
